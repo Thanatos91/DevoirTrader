249b0786292817b752bef6833578bb9aa9649468
Thanatos91 <fhachemi@lerebours.org>
2019-10-02 14:38:29 +0200

    Création des classes

---
+++ ModelObjet/ActionAchetee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelObjet
+{
+    public class ActionAchetee
+    {
+        public string CodeAction { get; set; }
+        public string NomAction { get; set; }
+        public double ValeurAction { get; set; }
+        public int Quantite { get; set; }
+        public double PrixAchat { get; set; }
+    }
+}
